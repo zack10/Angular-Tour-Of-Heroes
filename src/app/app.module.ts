1139d096f4e4a8095b30ac8bcaea36050959c5fd
Zakarya EL BAZY <zakarya.elbazy@sofrecom.com>
2019-11-15 12:07:43 +0100

    switching from in memory data service to fetching data from remote server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,9 +31,9 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
     // Remove it when a real server is ready to receive requests.
-    HttpClientInMemoryWebApiModule.forRoot(
+    /*HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, { dataEncapsulation: false }
-    )
+    )*/
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,7 +12,7 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
-  private heroesUrl = 'api/heroes';  // URL to web api
+  private heroesUrl = 'http://localhost:8080/sirhus/api/heroes/';  // URL to web api
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
